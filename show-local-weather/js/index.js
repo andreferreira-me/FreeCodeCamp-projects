5e5791c09efa1fc1bcd4772bd0acf57e4eae2199
André Ferreira <contato@andreferreira.me>
2017-03-11 23:23:42 -0300

    Updated Show Local Weather

--- show-local-weather/js/index.js
+++ show-local-weather/js/index.js
@@ -1,53 +1,88 @@
-var temp = "F";
+var unit;
 
 $(document).ready(function() {
-	getLocation();
+	  unit = "°F";
+		$('#unit').text(unit);
+
+    getLocation();
 });
 
-function changeTemp() {
-	switch (temp) {
-		case "F":
-			var tempF = $("#temp").html();
-			var tempC = (tempF - 32) / 1.8;
-			$("#temp").text(tempC);
-			temp = "C";
-			break;
-		case "C":
-			var tempC = $("#temp").html();
-			var tempF = tempC * 9/5 + 32;
-			$("#temp").text(tempF);
-			temp = "F";
-			break;
-	}
+function FahrenheitToCelsius(tempF){
+	return ((tempF - 32) / 1.8).toFixed(2);
+}
+
+function CelsiusToFahrenheit(tempC){
+	return (tempC * 9 / 5 + 32).toFixed(2);
+}
+
+function changeUnit() {
+    switch (unit) {
+        case "°F":
+            var tempF = $("#temp").html();
+            $("#temp").text(FahrenheitToCelsius(tempF));
+
+						unit = "°C";
+						$("#unitButton").text("Convert to Fahrenheit");
+            break;
+
+        case "°C":
+            tempC = $("#temp").html();
+            $("#temp").text(CelsiusToFahrenheit(tempC));
+
+						unit = "°F";
+						$("#unitButton").text("Convert to Celsius");
+            break;
+    }
+
+		$("#unit").text(unit);
 }
 
 function currentWeather(position) {
-	var lat = position.coords.latitude;
-	var lon = position.coords.longitude;
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var apiKey = 'b288b9757522f71ffd04333fb95b7b80';
 
-	// POST Request a new quote from Mashape.com API
-	var output = $.ajax({
-		url: 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&APPID=20b9cb396f4ed0b2a317704d62753e6c',
-		type: 'POST',
-		data: {},
-		dataType: 'json',
-		success: function(data) {
-			$("#city").text(data.name);
-			$("#condition").attr('src', 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png');
-			$("#weather").text(data.weather[0].main);
-			$("#temp").text(data.main.temp);
-		},
-		// Trigger an alert when there is an error on request
-		error: function(err) {
-			alert(err);
-		}
-	});
+    $.ajax({
+        type: 'GET',
+        url: "http://api.openweathermap.org/data/2.5/weather?type=like&units=imperial"+
+						"&lat=" + lat + "&lon=" + lon + "&&APPID=" + apiKey,
+        success: function(data) {
+					if(data.cod == "404"){
+						alert(data.message + " Please, reload the page and try again.");
+					}else{
+
+            var desc = data.weather[0].description.split(" ");
+
+            $.each(desc, function(i, word) {
+                desc[i] = word.charAt(0).toUpperCase() + word.slice(1);
+            });
+
+            desc = desc.join(" ");
+
+            $('#city').text(data.name);
+
+            $('#temp').text(data.main.temp.toFixed(2));
+
+            $('#weatherDesc').text(desc);
+            $('#icon').attr('alt', desc);
+
+            $('#icon').attr('src', "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+					}
+        },
+        error: function(data) {
+            alert("Error to connect to weather API. Please, reload the page and try again.")
+        }
+    });
 }
 
 function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(currentWeather);
-	} else {
-		alert("Geolocation is not supported by this browser.");
-	}
+  function geo_success(position) {
+    currentWeather(position);
+  }
+
+  function geo_error() {
+    alert("Geolocation is not supported by this browser.");
+  }
+
+  navigator.geolocation.getCurrentPosition(geo_success, geo_error);
 }
